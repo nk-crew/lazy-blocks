ea4accd88bd8eb2e646a894eb39b3746ebcdd09e
Nikita <nkdevinfo@gmail.com>
2019-05-23 11:06:47 +0300

    fixed controls is not saving in new blocks constructor

--- src/assets/admin/js/store/block-data/actions.jsx
+++ src/assets/admin/js/store/block-data/actions.jsx
@@ -7,6 +7,13 @@ export function apiFetch( request ) {
 }
 
 export function setBlockData( postId, data ) {
+    // check if control is array and change it to object
+    // unless value will not be saved.
+    // related topic: https://wordpress.org/support/topic/controls-not-saving/
+    if ( data.controls && typeof data.controls === 'object' && data.controls.constructor === Array ) {
+        data.controls = {};
+    }
+
     return {
         type: 'SET_BLOCK_DATA',
         post_id: postId,
