5b85245ca2dd43cc1ff68dac0002009690d2f66a
Nikita <nkdevinfo@gmail.com>
2019-06-26 12:45:30 +0300

    fixed code component bottom margin

--- src/assets/admin/js/components/tabs/index.jsx
+++ src/assets/admin/js/components/tabs/index.jsx
@@ -3,18 +3,21 @@ import './editor.scss';
 
 const { Component } = wp.element;
 const {
+    BaseControl,
     TabPanel,
 } = wp.components;
 
 export default class Tabs extends Component {
     render() {
         return (
-            <TabPanel
-                tabs={ this.props.tabs }
-                className="lazyblocks-component-tabs"
-            >
-                { this.props.children }
-            </TabPanel>
+            <BaseControl>
+                <TabPanel
+                    tabs={ this.props.tabs }
+                    className="lazyblocks-component-tabs"
+                >
+                    { this.props.children }
+                </TabPanel>
+            </BaseControl>
         );
     }
 }
