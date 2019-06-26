1ac72e7860347e294965ba9bf08b86b7744f9e8e
Nikita <nkdevinfo@gmail.com>
2019-06-26 14:14:27 +0300

    updated wpeg config

--- wpeg.config.js
+++ wpeg.config.js
@@ -8,7 +8,7 @@ cfg.dist_root = './dist';
 cfg.dist = '{dist_root}/lazy-blocks';
 
 // Template variables that will be automatically replaced.
-cfg.template_files_src = '{src}/**/*.{php,js,css}';
+cfg.template_files_src = '{src}/**/*.{md,php,js,css}';
 cfg.template_files_variables = {
     text_domain: pkg.name,
     plugin_version: pkg.version,
