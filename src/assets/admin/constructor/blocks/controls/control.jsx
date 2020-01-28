870c05990c35e5eae0709ded46c8040fcfe7e7d3
Nikita <nkdevinfo@gmail.com>
2020-01-28 14:46:44 +0300

    better structure for contructor control
    fixed constructor hover buttons in repeater childs

--- src/assets/admin/constructor/blocks/controls/control.jsx
+++ src/assets/admin/constructor/blocks/controls/control.jsx
@@ -92,69 +92,67 @@ class Control extends Component {
                 } }
                 role="none"
             >
-                <div className="lzb-constructor-controls-item-head">
-                    <div className="lzb-constructor-controls-item-icon">
-                        <span dangerouslySetInnerHTML={ { __html: controlTypeData.icon } } />
-                        <DragHandle />
-                    </div>
-                    <div className="lzb-constructor-controls-item-label">
-                        <span className="lzb-constructor-controls-item-label-text">
-                            { label || placeholder || <span>&nbsp;</span> }
-                            { 'true' === required ? (
-                                <span className="required">*</span>
-                            ) : '' }
-                        </span>
-                        <span className="lzb-constructor-controls-item-label-buttons">
-                            <button
-                                onClick={ ( e ) => {
-                                    e.preventDefault();
-                                    e.stopPropagation();
-
-                                    const newData = merge( {}, data );
-
-                                    newData.label += ' ' + __( '(copy)', '@@text_domain' );
-                                    newData.name += '-copy';
-
-                                    addControl( newData, id );
-                                } }
-                            >
-                                { __( 'Duplicate', '@@text_domain' ) }
-                            </button>
-                            <button
-                                className="lzb-constructor-controls-item-label-buttons-remove"
-                                onClick={ ( e ) => {
-                                    e.preventDefault();
-                                    e.stopPropagation();
-
-                                    // eslint-disable-next-line
-                                    if ( window.confirm( __( 'Do you really want to remove control?', '@@text_domain' ) ) ) {
-                                        removeControl();
-                                    }
-                                } }
-                            >
-                                { __( 'Remove', '@@text_domain' ) }
-                            </button>
-                        </span>
-                    </div>
-                    { ! data.child_of && controlName ? (
+                <div className="lzb-constructor-controls-item-icon">
+                    <span dangerouslySetInnerHTML={ { __html: controlTypeData.icon } } />
+                    <DragHandle />
+                </div>
+                <div className="lzb-constructor-controls-item-label">
+                    <span className="lzb-constructor-controls-item-label-text">
+                        { label || placeholder || <span>&nbsp;</span> }
+                        { 'true' === required ? (
+                            <span className="required">*</span>
+                        ) : '' }
+                    </span>
+                    <span className="lzb-constructor-controls-item-label-buttons">
                         <button
-                            className="lzb-constructor-controls-item-name"
                             onClick={ ( e ) => {
                                 e.preventDefault();
                                 e.stopPropagation();
-                                this.copyName( controlName );
+
+                                const newData = merge( {}, data );
+
+                                newData.label += ' ' + __( '(copy)', '@@text_domain' );
+                                newData.name += '-copy';
+
+                                addControl( newData, id );
                             } }
                         >
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1H4C2.9 1 2 1.9 2 3V17H4V3H16V1ZM15 5H8C6.9 5 6.01 5.9 6.01 7L6 21C6 22.1 6.89 23 7.99 23H19C20.1 23 21 22.1 21 21V11L15 5ZM8 21V7H14V12H19V21H8Z" fill="currentColor" /></svg>
-                            { controlName }
-                            { this.state.copied ? (
-                                <span className="lzb-constructor-controls-item-name-copied">
-                                    { __( 'Copied!', '@@text_domain' ) }
-                                </span>
-                            ) : '' }
+                            { __( 'Duplicate', '@@text_domain' ) }
                         </button>
-                    ) : '' }
+                        <button
+                            className="lzb-constructor-controls-item-label-buttons-remove"
+                            onClick={ ( e ) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+
+                                // eslint-disable-next-line
+                                if ( window.confirm( __( 'Do you really want to remove control?', '@@text_domain' ) ) ) {
+                                    removeControl();
+                                }
+                            } }
+                        >
+                            { __( 'Remove', '@@text_domain' ) }
+                        </button>
+                    </span>
                 </div>
+                { ! data.child_of && controlName ? (
+                    <button
+                        className="lzb-constructor-controls-item-name"
+                        onClick={ ( e ) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            this.copyName( controlName );
+                        } }
+                    >
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1H4C2.9 1 2 1.9 2 3V17H4V3H16V1ZM15 5H8C6.9 5 6.01 5.9 6.01 7L6 21C6 22.1 6.89 23 7.99 23H19C20.1 23 21 22.1 21 21V11L15 5ZM8 21V7H14V12H19V21H8Z" fill="currentColor" /></svg>
+                        { controlName }
+                        { this.state.copied ? (
+                            <span className="lzb-constructor-controls-item-name-copied">
+                                { __( 'Copied!', '@@text_domain' ) }
+                            </span>
+                        ) : '' }
+                    </button>
+                ) : '' }
             </div>
         ), this.props );
 
