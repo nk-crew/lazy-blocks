7ad89c00dc2557b907206b4e069e6869835c9aba
Nikita <nkdevinfo@gmail.com>
2020-02-01 23:22:28 +0300

    fixed placement restrictions condition in constructor

--- src/assets/admin/constructor/blocks/selected-control-settings/settings-rows/placement.jsx
+++ src/assets/admin/constructor/blocks/selected-control-settings/settings-rows/placement.jsx
@@ -42,52 +42,50 @@ export default class PlacementRow extends Component {
                 >
                     <div />
                     <ButtonGroup>
-                        { 'content'.indexOf( placementRestrictions ) === -1 ? (
-                            <Button
-                                isDefault
-                                isPrimary={ 'content' === placement || 'both' === placement }
-                                onClick={ () => {
-                                    let newPlacement = 'content';
+                        <Button
+                            isDefault
+                            isPrimary={ 'content' === placement || 'both' === placement }
+                            disabled={ placementRestrictions.indexOf( 'content' ) === -1 }
+                            onClick={ () => {
+                                let newPlacement = 'content';
 
-                                    if ( 'both' === placement ) {
-                                        newPlacement = 'inspector';
-                                    } else if ( 'content' === placement ) {
-                                        newPlacement = 'nowhere';
-                                    } else if ( 'inspector' === placement ) {
-                                        newPlacement = 'both';
-                                    }
+                                if ( 'both' === placement ) {
+                                    newPlacement = 'inspector';
+                                } else if ( 'content' === placement ) {
+                                    newPlacement = 'nowhere';
+                                } else if ( 'inspector' === placement ) {
+                                    newPlacement = 'both';
+                                }
 
-                                    updateData( {
-                                        placement: newPlacement,
-                                    } );
-                                } }
-                            >
-                                { __( 'Content', '@@text_domain' ) }
-                            </Button>
-                        ) : '' }
-                        { 'inspector'.indexOf( placementRestrictions ) === -1 ? (
-                            <Button
-                                isDefault
-                                isPrimary={ 'inspector' === placement || 'both' === placement }
-                                onClick={ () => {
-                                    let newPlacement = 'inspector';
+                                updateData( {
+                                    placement: newPlacement,
+                                } );
+                            } }
+                        >
+                            { __( 'Content', '@@text_domain' ) }
+                        </Button>
+                        <Button
+                            isDefault
+                            isPrimary={ 'inspector' === placement || 'both' === placement }
+                            disabled={ placementRestrictions.indexOf( 'inspector' ) === -1 }
+                            onClick={ () => {
+                                let newPlacement = 'inspector';
 
-                                    if ( 'both' === placement ) {
-                                        newPlacement = 'content';
-                                    } else if ( 'inspector' === placement ) {
-                                        newPlacement = 'nowhere';
-                                    } else if ( 'content' === placement ) {
-                                        newPlacement = 'both';
-                                    }
+                                if ( 'both' === placement ) {
+                                    newPlacement = 'content';
+                                } else if ( 'inspector' === placement ) {
+                                    newPlacement = 'nowhere';
+                                } else if ( 'content' === placement ) {
+                                    newPlacement = 'both';
+                                }
 
-                                    updateData( {
-                                        placement: newPlacement,
-                                    } );
-                                } }
-                            >
-                                { __( 'Inspector', '@@text_domain' ) }
-                            </Button>
-                        ) : '' }
+                                updateData( {
+                                    placement: newPlacement,
+                                } );
+                            } }
+                        >
+                            { __( 'Inspector', '@@text_domain' ) }
+                        </Button>
                     </ButtonGroup>
                 </BaseControl>
             </PanelBody>
