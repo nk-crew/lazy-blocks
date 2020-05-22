9a1d6a1a9a65c9f485fc22b7448a039a2cc4839b
Nikita <nkdevinfo@gmail.com>
2020-05-22 15:31:31 +0300

    added help link to "Save in Meta" control option, as users just enable it without needing

--- src/assets/admin/constructor/blocks/selected-control-settings/settings-rows/save-in-meta.jsx
+++ src/assets/admin/constructor/blocks/selected-control-settings/settings-rows/save-in-meta.jsx
@@ -5,6 +5,7 @@ const { Component, Fragment } = wp.element;
 const {
     PanelBody,
     BaseControl,
+    Button,
     CheckboxControl,
     TextControl,
 } = wp.components;
@@ -20,7 +21,19 @@ export default class SaveInMetaRow extends Component {
             <Fragment>
                 <PanelBody>
                     <BaseControl
-                        label={ __( 'Save in meta', '@@text_domain' ) }
+                        label={ __( 'Save in Meta', '@@text_domain' ) }
+                        help={ (
+                            <Button
+                                isSecondary
+                                isSmall
+                                href="https://lazyblocks.com/documentation/examples/display-custom-fields-meta/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                style={ { marginTop: '10px' } }
+                            >
+                                { __( 'How to use?', '@@text_domain' ) }
+                            </Button>
+                        ) }
                     >
                         <CheckboxControl
                             label={ __( 'Yes', '@@text_domain' ) }
@@ -28,17 +41,15 @@ export default class SaveInMetaRow extends Component {
                             onChange={ ( value ) => updateData( { save_in_meta: value ? 'true' : 'false' } ) }
                         />
                     </BaseControl>
-                </PanelBody>
-                { 'true' === data.save_in_meta ? (
-                    <PanelBody>
+                    { 'true' === data.save_in_meta ? (
                         <TextControl
                             label={ __( 'Meta custom name (optional)', '@@text_domain' ) }
                             value={ data.save_in_meta_name }
                             onChange={ ( value ) => updateData( { save_in_meta_name: value } ) }
                             placeholder={ data.name || __( 'Unique metabox name', '@@text_domain' ) }
                         />
-                    </PanelBody>
-                ) : '' }
+                    ) : '' }
+                </PanelBody>
             </Fragment>
         );
     }
