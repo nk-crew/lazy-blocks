8f7bbb1e15b7b234614737e92cada2cd7e8e20a1
Nikita <nkdevinfo@gmail.com>
2020-06-27 23:59:00 +0300

    added Reset button to URL control

--- src/controls/url/script.jsx
+++ src/controls/url/script.jsx
@@ -1,34 +1,66 @@
+import shorthash from 'shorthash';
+
+const {
+    useState,
+} = wp.element;
+
+const {
+    __,
+} = wp.i18n;
+
 const {
     addFilter,
 } = wp.hooks;
 
 const {
     BaseControl,
+    Button,
 } = wp.components;
 
 const {
     __experimentalLinkControl: LinkControl,
 } = wp.blockEditor;
 
+function ComponentRender( props ) {
+    const [ key, setKey ] = useState( shorthash.unique( `${ new Date() }` ) );
+
+    return (
+        <BaseControl
+            label={ props.data.label }
+            help={ props.data.help }
+        >
+            <div className="lzb-gutenberg-url">
+                <LinkControl
+                    key={ key }
+                    className="wp-block-navigation-link__inline-link-input"
+                    opensInNewTab={ false }
+                    value={ {
+                        url: props.getValue(),
+                    } }
+                    onChange={ ( { url: newURL = '' } ) => {
+                        props.onChange( newURL );
+                    } }
+                />
+                { props.getValue() ? (
+                    <Button
+                        isSmall
+                        isSecondary
+                        onClick={ () => {
+                            props.onChange( '' );
+                            setKey( shorthash.unique( `${ new Date() }` ) );
+                        } }
+                    >
+                        { __( 'Reset', '@@text_domain' ) }
+                    </Button>
+                ) : '' }
+            </div>
+        </BaseControl>
+    );
+}
+
 /**
  * Control render in editor.
  */
 addFilter( 'lzb.editor.control.url.render', 'lzb.editor', ( render, props ) => (
-    <BaseControl
-        label={ props.data.label }
-        help={ props.data.help }
-    >
-        <div className="lzb-gutenberg-url">
-            <LinkControl
-                className="wp-block-navigation-link__inline-link-input"
-                opensInNewTab={ false }
-                value={ {
-                    url: props.getValue(),
-                } }
-                onChange={ ( { url: newURL = '' } ) => {
-                    props.onChange( newURL );
-                } }
-            />
-        </div>
-    </BaseControl>
+    <ComponentRender { ...props } />
 ) );
