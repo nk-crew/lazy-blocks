1b72491a4d28109150e1eb1763096a60aeb02f8c
Nikita <nkdevinfo@gmail.com>
2021-12-06 01:35:42 +0300

    added constructor code preview iframe resize and changed error placement

--- src/assets/admin/constructor/blocks/code-preview/index.js
+++ src/assets/admin/constructor/blocks/code-preview/index.js
@@ -19,14 +19,16 @@ const {
 class CodePreview extends Component {
     constructor( ...args ) {
         super( ...args );
+
         this.state = {
-            codePreview: null,
-            error: null,
+            codePreview: '',
         };
 
         this.loadPreview = throttle( 2000, this.loadPreview.bind( this ) );
         this.iframeRef = createRef();
         this.setIframeContents = this.setIframeContents.bind( this );
+        this.setIframeHeight = this.setIframeHeight.bind( this );
+        this.calculateIframeHeight = this.calculateIframeHeight.bind( this );
     }
 
     componentDidMount() {
@@ -42,6 +44,13 @@ class CodePreview extends Component {
         if ( prevProps.data !== data || prevProps.tab !== tab ) {
             this.loadPreview();
         }
+    }
+
+    componentWillUnmount() {
+        const iframe = this.iframeRef.current;
+
+        this.iframeObserver.disconnect();
+        iframe.removeEventListener( 'load', this.setIframeHeight );
     }
 
     getAttributes() {
@@ -61,22 +70,62 @@ class CodePreview extends Component {
         return result;
     }
 
-    setIframeContents( frame ) {
+    setIframeContents() {
         const {
             codePreview,
         } = this.state;
 
-        if ( frame ) {
-            this.iframeRef.current = frame;
+        return `
+            <link rel="stylesheet" href="/wp-admin/load-styles.php?load[chunk_0]=common,media,themes" type="text/css" media="all" />
+            <style>
+                html {
+                    height: auto;
+                }
+                body {
+                    background: #fff !important;
+                    overflow: auto;
+                }
+            </style>
+            ${ codePreview }
+        `;
+    }
 
-            if ( codePreview ) {
-                const doc = frame.contentDocument;
-                doc.open();
-                doc.write( `<link rel="stylesheet" href="/wp-admin/load-styles.php?load[chunk_0]=common,media,themes" type="text/css" media="all" /><style>body {background: #fff !important;}</style>${ codePreview }` );
-                doc.close();
-                frame.style.width = '100%';
-            }
+    setIframeHeight() {
+        const iframe = this.iframeRef.current;
+
+        // Pick the maximum of these two values to account for margin collapsing.
+        const height = Math.max(
+            iframe.contentDocument.documentElement.offsetHeight,
+            iframe.contentDocument.body.offsetHeight
+        );
+
+        iframe.style.height = `${ height }px`;
+    }
+
+    calculateIframeHeight( iframe ) {
+        if ( ! iframe ) {
+            return;
         }
+
+        this.iframeRef.current = iframe;
+
+        const { IntersectionObserver } = iframe.ownerDocument.defaultView;
+
+        // Observe for intersections that might cause a change in the height of
+        // the iframe, e.g. a Widget Area becoming expanded.
+        this.iframeObserver = new IntersectionObserver(
+            ( [ entry ] ) => {
+                if ( entry.isIntersecting ) {
+                    this.setIframeHeight();
+                }
+            },
+            {
+                threshold: 1,
+            }
+        );
+        this.iframeObserver.observe( iframe );
+
+        iframe.addEventListener( 'load', this.setIframeHeight );
     }
 
     loadPreview() {
@@ -100,33 +149,32 @@ class CodePreview extends Component {
                 .then( ( { response, error, error_code: errorCode } ) => {
                     if ( error ) {
                         if ( 'lazy_block_no_render_callback' === errorCode ) {
-                            this.setState( { codePreview: null, error: '' } );
+                            this.setState( { codePreview: '' } );
                         } else {
-                            this.setState( { codePreview: null, error: response } );
+                            this.setState( { codePreview: `<pre>${ response }</pre>` } );
                         }
                     } else {
-                        this.setState( { codePreview: response, error: null }, () => this.setIframeContents( this.iframeRef.current ) );
+                        this.setState( { codePreview: response } );
                     }
                 } ).catch( () => {
-                    this.setState( { codePreview: null, error: 'Error' } );
+                    this.setState( { codePreview: __( 'Error: Could not generate the preview.', '@@text_domain' ) } );
                 } );
         }
     }
 
     render() {
-        const {
-            error,
-            codePreview,
-        } = this.state;
-
         return (
             <div className="lazyblocks-component-box lazyblocks-component-box-no-paddings lzb-constructor-code-preview">
                 <PanelBody>
                     <h2>{ __( 'Preview', '@@text_domain' ) }</h2>
                 </PanelBody>
                 <PanelBody>
-                    { error && <code><pre className="lzb-error-box">{ error }</pre></code> }
-                    <iframe src="about:blank" frameBorder="0" style={ error || null == codePreview ? { display: 'none' } : {} } ref={ this.setIframeContents } title="code-preview" />
+                    <iframe
+                        srcDoc={ this.setIframeContents() }
+                        ref={ this.calculateIframeHeight }
+                        frameBorder="0"
+                        title="code-preview"
+                    />
                 </PanelBody>
             </div>
         );
