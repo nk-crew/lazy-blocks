4fc103963c4f8862c62e0c21bb4d55c4dc7b553c
Nikita <nkdevinfo@gmail.com>
2022-10-29 21:56:03 +0300

    improved color palettes in the color picker

--- controls/color/color-control.js
+++ controls/color/color-control.js
@@ -2,19 +2,39 @@
 
 const { BaseControl, ColorPalette } = wp.components;
 
+const { __experimentalUseMultipleOriginColorsAndGradients: useMultipleOriginColorsAndGradients } =
+  wp.blockEditor;
+
 const { useSelect } = wp.data;
+
+function useColors() {
+  // New way to get colors and gradients.
+  if (useMultipleOriginColorsAndGradients && useMultipleOriginColorsAndGradients()) {
+    return useMultipleOriginColorsAndGradients().colors;
+  }
+
+  // Old way.
+  const { themeColors } = useSelect((select) => {
+    const settings = select('core/block-editor').getSettings();
+
+    return {
+      themeColors: settings.colors,
+    };
+  });
+
+  const colors = [];
+
+  if (themeColors && themeColors.length) {
+    colors.push({ name: 'Theme', colors: themeColors });
+  }
+
+  return colors;
+}
 
 function ColorControl(props) {
   const { label, value, help, alpha = false, onChange = () => {} } = props;
 
-  const { colors } = useSelect((select) => {
-    const settings = select('core/block-editor').getSettings();
-    const result = props.colors === undefined ? settings.colors : props.colors;
-
-    return {
-      colors: result,
-    };
-  });
+  const colors = useColors();
 
   return (
     <BaseControl label={label} help={help} className="lzb-gutenberg-color">
@@ -25,6 +45,8 @@ function ColorControl(props) {
         onChange={(val) => {
           onChange(val);
         }}
+        __experimentalHasMultipleOrigins
+        __experimentalIsRenderedInSidebar
       />
     </BaseControl>
   );
