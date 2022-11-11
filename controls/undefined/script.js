01098690b055a0a0c49e878e806d0d81975e6656
Nikita <nkdevinfo@gmail.com>
2022-11-11 23:44:20 +0300

    cleaned script for undefined control

--- controls/undefined/script.js
+++ controls/undefined/script.js
@@ -1,31 +1,19 @@
 const { __ } = wp.i18n;
 
-const { Fragment } = wp.element;
-
 const { addFilter } = wp.hooks;
 
-const { PanelBody, TextControl } = wp.components;
+const { PanelBody } = wp.components;
 
 /**
  * Control render in editor.
  */
-addFilter('lzb.editor.control.undefined.render', 'lzb.editor', (render, props) => {
-  console.log(props);
-
+addFilter('lzb.editor.control.undefined.render', 'lzb.editor', () => {
   return <div>{__('Looks like this control does not exists.', 'lazy-blocks')}</div>;
 });
 
 /**
  * Control settings render in constructor.
  */
-addFilter('lzb.constructor.control.undefined.settings', 'lzb.constructor', (render, props) => {
-  const { updateData, data } = props;
-
-  console.log(data);
-
-  return (
-    <Fragment>
-      <PanelBody>{__('Looks like this control does not exists.', 'lazy-blocks')}</PanelBody>
-    </Fragment>
-  );
+addFilter('lzb.constructor.control.undefined.settings', 'lzb.constructor', () => {
+  return <PanelBody>{__('Looks like this control does not exists.', 'lazy-blocks')}</PanelBody>;
 });
