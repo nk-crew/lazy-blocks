5c249137f93bbe46e93c2f00059ffde5bb86b59f
Nikita <nkdevinfo@gmail.com>
2022-12-02 16:01:09 +0300

    changed classic editor control to functional component

--- controls/classic_editor/script.js
+++ controls/classic_editor/script.js
@@ -7,19 +7,25 @@ const { addFilter } = wp.hooks;
 /**
  * Control render in editor.
  */
-addFilter('lzb.editor.control.classic_editor.render', 'lzb.editor', (render, props, blockProps) => (
-  <BaseControl
-    key={props.data.name}
-    label={props.data.label}
-    help={props.data.help}
-    className="lzb-gutenberg-classic-editor"
-  >
-    <TMCE
-      content={props.getValue()}
-      onChange={(val) => {
-        props.onChange(val);
-      }}
-      editorId={props.placement + blockProps.clientId + props.uniqueId + (props.childIndex || 0)}
-    />
-  </BaseControl>
-));
+addFilter('lzb.editor.control.classic_editor.render', 'lzb.editor', (render, props, blockProps) => {
+  const uniqueKey = `${props.placement}-${blockProps.clientId}-${props.uniqueId}-${
+    props.data.name
+  }-${props.childIndex || 0}`;
+
+  return (
+    <BaseControl
+      key={uniqueKey}
+      label={props.data.label}
+      help={props.data.help}
+      className="lzb-gutenberg-classic-editor"
+    >
+      <TMCE
+        content={props.getValue()}
+        onChange={(val) => {
+          props.onChange(val);
+        }}
+        editorId={uniqueKey}
+      />
+    </BaseControl>
+  );
+});
--- controls/classic_editor/tmce.js
+++ controls/classic_editor/tmce.js
@@ -1,202 +1,189 @@
+/* eslint-disable no-underscore-dangle */
 /**
  * TinyMCE Component from https://github.com/WordPress/gutenberg/blob/trunk/packages/block-library/src/freeform/edit.js
  * With changes to work as control.
  */
 
 /**
+ * External dependencies
+ */
+import { debounce } from 'throttle-debounce';
+
+/**
  * WordPress dependencies
  */
 const { wp } = window;
 
-const { Component } = wp.element;
+const { useEffect, useRef } = wp.element;
 
 const { __ } = wp.i18n;
 
-const { BACKSPACE, DELETE, F10 } = wp.keycodes;
+const { F10, isKeyboardEvent } = wp.keycodes;
 
-function isTmceEmpty(editor) {
-  // When tinyMce is empty the content seems to be:
-  // <p><br data-mce-bogus="1"></p>
-  // avoid expensive checks for large documents
-  const body = editor.getBody();
-  if (1 < body.childNodes.length) {
-    return false;
-  }
-  if (0 === body.childNodes.length) {
-    return true;
-  }
-  if (1 < body.childNodes[0].childNodes.length) {
-    return false;
-  }
-  return /^\n?$/.test(body.innerText || body.textContent);
-}
+export default function ClassicEdit(props) {
+  const { editorId, content, onChange } = props;
 
-export default class ClassicEdit extends Component {
-  constructor(props) {
-    super(props);
-    this.initialize = this.initialize.bind(this);
-    this.onSetup = this.onSetup.bind(this);
-    this.focus = this.focus.bind(this);
-  }
+  const didMount = useRef(false);
 
-  componentDidMount() {
-    const { baseURL, suffix } = window.wpEditorL10n ? window.wpEditorL10n.tinymce : window.tinymce;
+  useEffect(() => {
+    if (!didMount.current) {
+      return;
+    }
+
+    const editor = window.tinymce.get(`editor-${editorId}`);
+    const currentContent = editor?.getContent();
+
+    if (currentContent !== content) {
+      editor.setContent(content || '');
+    }
+  }, [content]);
+
+  useEffect(() => {
+    const { baseURL, suffix } = window.wpEditorL10n.tinymce;
+    // const { baseURL, suffix } = window.wpEditorL10n ? window.wpEditorL10n.tinymce : window.tinymce;
+
+    didMount.current = true;
 
     window.tinymce.EditorManager.overrideDefaults({
       base_url: baseURL,
       suffix,
     });
 
-    if ('complete' === document.readyState) {
-      this.initialize();
-    } else {
-      document.addEventListener('readystatechange', this.initialize);
-    }
-  }
+    function onSetup(editor) {
+      let bookmark;
 
-  componentDidUpdate(prevProps) {
-    const { content, onChange } = this.props;
+      if (content) {
+        editor.on('loadContent', () => editor.setContent(content));
+      }
 
-    if (prevProps.content !== content) {
-      onChange(content || '');
-    }
-  }
+      editor.on('blur', () => {
+        bookmark = editor.selection.getBookmark(2, true);
+        // There is an issue with Chrome and the editor.focus call in core at https://core.trac.wordpress.org/browser/trunk/src/js/_enqueues/lib/link.js#L451.
+        // This causes a scroll to the top of editor content on return from some content updating dialogs so tracking
+        // scroll position until this is fixed in core.
+        const scrollContainer = document.querySelector('.interface-interface-skeleton__content');
+        const scrollPosition = scrollContainer.scrollTop;
 
-  componentWillUnmount() {
-    document.removeEventListener('readystatechange', this.initialize);
-    wp.oldEditor.remove(`editor-${this.props.editorId}`);
-  }
+        editor.once('focus', () => {
+          if (bookmark) {
+            editor.selection.moveToBookmark(bookmark);
+            if (scrollContainer.scrollTop !== scrollPosition) {
+              scrollContainer.scrollTop = scrollPosition;
+            }
+          }
+        });
 
-  onSetup(editor) {
-    const { content, onChange } = this.props;
+        return false;
+      });
 
-    const { ref } = this;
-    let bookmark;
+      editor.on('mousedown touchstart', () => {
+        bookmark = null;
+      });
 
-    this.editor = editor;
+      const debouncedOnChange = debounce(250, () => {
+        const value = editor.getContent();
 
-    if (content) {
-      editor.on('loadContent', () => editor.setContent(content));
-    }
+        if (value !== editor._lastChange) {
+          editor._lastChange = value;
+          onChange(value);
+        }
+      });
+      editor.on('Paste Change input Undo Redo', debouncedOnChange);
 
-    editor.on('blur', () => {
-      bookmark = editor.selection.getBookmark(2, true);
+      // We need to cancel the debounce call because when we remove
+      // the editor (onUnmount) this callback is executed in
+      // another tick. This results in setting the content to empty.
+      editor.on('remove', debouncedOnChange.cancel);
 
-      onChange(editor.getContent());
+      editor.on('keydown', (event) => {
+        if (isKeyboardEvent.primary(event, 'z')) {
+          // Prevent the gutenberg undo kicking in so TinyMCE undo stack works as expected.
+          event.stopPropagation();
+        }
 
-      editor.once('focus', () => {
-        if (bookmark) {
-          editor.selection.moveToBookmark(bookmark);
+        const { altKey } = event;
+        /*
+         * Prevent Mousetrap from kicking in: TinyMCE already uses its own
+         * `alt+f10` shortcut to focus its toolbar.
+         */
+        if (altKey && event.keyCode === F10) {
+          event.stopPropagation();
         }
       });
 
-      return false;
-    });
+      editor.on('init', () => {
+        const rootNode = editor.getBody();
 
-    editor.on('change', () => {
-      onChange(editor.getContent());
-    });
+        // Create the toolbar by refocussing the editor.
+        if (rootNode.ownerDocument.activeElement === rootNode) {
+          rootNode.blur();
+          editor.focus();
+        }
+      });
+    }
 
-    editor.on('mousedown touchstart', () => {
-      bookmark = null;
-    });
+    function initialize() {
+      const { settings } = window.wpEditorL10n.tinymce;
+      wp.oldEditor.initialize(`editor-${editorId}`, {
+        tinymce: {
+          ...settings,
+          inline: true,
+          content_css: false,
+          fixed_toolbar_container: `#toolbar-${editorId}`,
+          setup: onSetup,
+        },
+      });
+    }
 
-    editor.on('keydown', (event) => {
-      if ((event.keyCode === BACKSPACE || event.keyCode === DELETE) && isTmceEmpty(editor)) {
-        // delete the block
-        this.props.onReplace([]);
-        event.preventDefault();
-        event.stopImmediatePropagation();
+    function onReadyStateChange() {
+      if ('complete' === document.readyState) {
+        initialize();
       }
+    }
 
-      const { altKey } = event;
-      /*
-       * Prevent Mousetrap from kicking in: TinyMCE already uses its own
-       * `alt+f10` shortcut to focus its toolbar.
-       */
-      if (altKey && event.keyCode === F10) {
-        event.stopPropagation();
-      }
-    });
+    if ('complete' === document.readyState) {
+      initialize();
+    } else {
+      document.addEventListener('readystatechange', onReadyStateChange);
+    }
 
-    // Show the second, third, etc. toolbars when the `kitchensink` button is removed by a plugin.
-    editor.on('init', () => {
-      if (editor.settings.toolbar1 && -1 === editor.settings.toolbar1.indexOf('kitchensink')) {
-        editor.dom.addClass(ref, 'has-advanced-toolbar');
-      }
-    });
+    return () => {
+      document.removeEventListener('readystatechange', onReadyStateChange);
+      wp.oldEditor.remove(`editor-${editorId}`);
+    };
+  }, []);
 
-    editor.addButton('wp_add_media', {
-      tooltip: __('Insert Media', 'lazy-blocks'),
-      icon: 'dashicon dashicons-admin-media',
-      cmd: 'WP_Medialib',
-    });
-    // End TODO.
-
-    editor.on('init', () => {
-      const rootNode = this.editor.getBody();
-
-      // Create the toolbar by refocussing the editor.
-      if (document.activeElement === rootNode) {
-        rootNode.blur();
-        this.editor.focus();
-      }
-    });
+  function focus() {
+    const editor = window.tinymce.get(`editor-${editorId}`);
+    if (editor) {
+      editor.focus();
+    }
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  onToolbarKeyDown(event) {
+  function onToolbarKeyDown(event) {
     // Prevent WritingFlow from kicking in and allow arrows navigation on the toolbar.
     event.stopPropagation();
     // Prevent Mousetrap from moving focus to the top toolbar when pressing `alt+f10` on this block toolbar.
     event.nativeEvent.stopImmediatePropagation();
   }
 
-  focus() {
-    if (this.editor) {
-      this.editor.focus();
-    }
-  }
+  // Disable reasons:
+  //
+  // jsx-a11y/no-static-element-interactions
+  //  - the toolbar itself is non-interactive, but must capture events
+  //    from the KeyboardShortcuts component to stop their propagation.
 
-  initialize() {
-    const { editorId } = this.props;
-    const { settings } = window.wpEditorL10n ? window.wpEditorL10n.tinymce : window.tinymce;
-
-    wp.oldEditor.initialize(`editor-${editorId}`, {
-      tinymce: {
-        ...settings,
-        inline: true,
-        content_css: false,
-        fixed_toolbar_container: `#toolbar-${editorId}`,
-        setup: this.onSetup,
-      },
-    });
-  }
-
-  render() {
-    const { editorId } = this.props;
-
-    // Disable reasons:
-    //
-    // jsx-a11y/no-static-element-interactions
-    //  - the toolbar itself is non-interactive, but must capture events
-    //    from the KeyboardShortcuts component to stop their propagation.
-
-    /* eslint-disable jsx-a11y/no-static-element-interactions */
-    return [
-      <div
-        key="toolbar"
-        id={`toolbar-${editorId}`}
-        ref={(ref) => {
-          this.ref = ref;
-        }}
-        className="block-library-classic__toolbar"
-        onClick={this.focus}
-        data-placeholder={__('Classic', 'lazy-blocks')}
-        onKeyDown={this.onToolbarKeyDown}
-      />,
-      <div key="editor" id={`editor-${editorId}`} className="block-library-rich-text__tinymce" />,
-    ];
-    /* eslint-enable jsx-a11y/no-static-element-interactions */
-  }
+  /* eslint-disable jsx-a11y/no-static-element-interactions */
+  return [
+    <div
+      key="toolbar"
+      id={`toolbar-${editorId}`}
+      className="block-library-classic__toolbar"
+      onClick={focus}
+      data-placeholder={__('Classic', 'lazy-blocks')}
+      onKeyDown={onToolbarKeyDown}
+    />,
+    <div key="editor" id={`editor-${editorId}`} className="block-library-rich-text__tinymce" />,
+  ];
+  /* eslint-enable jsx-a11y/no-static-element-interactions */
 }
