3b30266b9080e9a255c6657cd62290ec69ab7490
Nikita <nkdevinfo@gmail.com>
2023-01-02 18:52:28 +0400

    added notice for invalid control name

--- assets/editor-constructor/boxes/selected-control-settings/settings-rows/name.js
+++ assets/editor-constructor/boxes/selected-control-settings/settings-rows/name.js
@@ -1,20 +1,43 @@
 const { __ } = wp.i18n;
+const { useState, useEffect } = wp.element;
+const { PanelBody, TextControl, Notice } = wp.components;
 
-const { PanelBody, TextControl } = wp.components;
+function checkNameSlug(slug) {
+  return /^[A-Za-z0-9-_]*$/.test(slug);
+}
 
 export default function NameRow(props) {
+  const [isNameValid, setIsNameValid] = useState(true);
+
   const { updateData, data } = props;
 
   const { name = '' } = data;
+
+  useEffect(() => {
+    const isValid = checkNameSlug(name);
+
+    if (isValid !== isNameValid) {
+      setIsNameValid(isValid);
+    }
+  }, [name]);
 
   return (
     <PanelBody>
       <TextControl
         label={__('Name', 'lazy-blocks')}
-        help={__('Unique control name, no spaces. Underscores and dashes allowed', 'lazy-blocks')}
         value={name}
         onChange={(value) => updateData({ name: value })}
       />
+      {!isNameValid ? (
+        <Notice status="error" isDismissible={false} className="lzb-constructor-notice">
+          {__(
+            'Control name must include only alphanumeric characters, dashes or underscores. Example: my-control-name',
+            'lazy-blocks'
+          )}
+        </Notice>
+      ) : (
+        ''
+      )}
     </PanelBody>
   );
 }
