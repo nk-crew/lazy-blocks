1b506e85183eda82b2deb34f97bce15764a799fa
Nikita <nkdevinfo@gmail.com>
2023-02-27 10:58:03 +0300

    improved free block appender style in editor

--- assets/editor/blocks/free/edit.js
+++ assets/editor/blocks/free/edit.js
@@ -1,8 +1,23 @@
-const { useBlockProps, useInnerBlocksProps } = wp.blockEditor;
+const { useSelect } = wp.data;
+const { InnerBlocks, useBlockProps, useInnerBlocksProps } = wp.blockEditor;
 
-export default function BlockEdit() {
-  const props = useBlockProps({ className: 'lazyblocks-free' });
-  const innerBlockProps = useInnerBlocksProps(props, { templateLock: null });
+export default function BlockEdit({ clientId }) {
+  const { hasChildBlocks } = useSelect(
+    (select) => {
+      const { getBlockOrder } = select('core/block-editor');
 
-  return <div {...innerBlockProps} />;
+      return {
+        hasChildBlocks: getBlockOrder(clientId).length > 0,
+      };
+    },
+    [clientId]
+  );
+
+  const blockProps = useBlockProps({ className: 'lazyblocks-free' });
+  const innerBlocksProps = useInnerBlocksProps(blockProps, {
+    templateLock: null,
+    renderAppender: hasChildBlocks ? undefined : InnerBlocks.ButtonBlockAppender,
+  });
+
+  return <div {...innerBlocksProps} />;
 }
