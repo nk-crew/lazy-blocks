bde3082f739fdc112ce775668f104e51b7a37fec
Nikita <nkdevinfo@gmail.com>
2023-03-22 16:27:24 +0300

    fixed displaying label and help in controls, where these settings are disabled

--- assets/hooks/use-block-control-props/index.js
+++ assets/hooks/use-block-control-props/index.js
@@ -1,9 +1,12 @@
 import classnames from 'classnames/dedupe';
 
+import getControlTypeData from '../../utils/get-control-type-data';
+
 export default function useBlockControlProps(controlProps, { className, ...data } = {}) {
+  const controlTypeData = getControlTypeData(controlProps.data.type);
   const props = {
-    label: controlProps.data.label,
-    help: controlProps.data.help,
+    label: controlTypeData.restrictions.label_settings ? controlProps.data.label : false,
+    help: controlTypeData.restrictions.help_settings ? controlProps.data.help : false,
     className: classnames(
       `lazyblocks-control lazyblocks-control-${controlProps.data.type}`,
       className
