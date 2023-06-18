d9af5b4790032ef45629a9de70cec3372505ef32
Nikita <nkdevinfo@gmail.com>
2023-06-18 15:09:43 +0300

    added possibility to sort tags inside Select component

--- assets/components/select/index.js
+++ assets/components/select/index.js
@@ -1,13 +1,26 @@
-/* eslint-disable indent */
-import CreatableSelect from 'react-select/creatable';
-import AsyncSelect from 'react-select/async';
-import ReactSelect from 'react-select';
-import selectStyles from 'gutenberg-react-select-styles';
-
-// Import CSS
+/**
+ * Styles
+ */
 import './editor.scss';
 
+/**
+ * External dependencies
+ */
+import CreatableSelect from 'react-select/creatable';
+import AsyncSelect from 'react-select/async';
+import ReactSelect, { components } from 'react-select';
+import selectStyles from 'gutenberg-react-select-styles';
+
+import { closestCenter, DndContext } from '@dnd-kit/core';
+import { restrictToParentElement } from '@dnd-kit/modifiers';
+import { SortableContext, useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+import { arrayMoveImmutable } from 'array-move';
+
 export default function Select(props) {
+	const { value, onChange } = props;
+
 	let ThisSelect = ReactSelect;
 
 	if (props.isAsync) {
@@ -16,25 +29,92 @@ export default function Select(props) {
 		ThisSelect = CreatableSelect;
 	}
 
-	return (
-		<ThisSelect
-			menuPlacement="auto"
-			className="lazyblocks-component-select"
-			styles={selectStyles}
-			{...{
-				...props,
-				...(props.isTags
-					? {
+	// Tags + Sortable.
+	if (props.isTags) {
+		return (
+			<DndContext
+				modifiers={[restrictToParentElement]}
+				onDragEnd={(event) => {
+					const { active, over } = event;
+
+					if (active.id !== over.id) {
+						const oldIndex = value.findIndex(
+							(item) => item.value === active.id
+						);
+						const newIndex = value.findIndex(
+							(item) => item.value === over.id
+						);
+
+						onChange(arrayMoveImmutable(value, oldIndex, newIndex));
+					}
+				}}
+				collisionDetection={closestCenter}
+			>
+				<SortableContext items={value.map((o) => o.value)}>
+					<ThisSelect
+						menuPlacement="auto"
+						className="lazyblocks-component-select"
+						styles={selectStyles}
+						{...{
+							...props,
 							isMulti: true,
 							components: {
 								DropdownIndicator: () => null,
 								IndicatorSeparator: () => null,
 								ClearIndicator: () => null,
 								Menu: () => null,
+								MultiValue: (valProps) => {
+									const onMouseDown = (e) => {
+										e.preventDefault();
+										e.stopPropagation();
+									};
+									const innerProps = {
+										...valProps.innerProps,
+										onMouseDown,
+									};
+									const {
+										attributes,
+										listeners,
+										setNodeRef,
+										transform,
+										transition,
+									} = useSortable({
+										id: valProps.data.value,
+									});
+									const style = {
+										transform:
+											CSS.Translate.toString(transform),
+										transition,
+									};
+
+									return (
+										<div
+											style={style}
+											ref={setNodeRef}
+											{...attributes}
+											{...listeners}
+										>
+											<components.MultiValue
+												{...valProps}
+												innerProps={innerProps}
+											/>
+										</div>
+									);
+								},
 							},
-					  }
-					: {}),
-			}}
+						}}
+					/>
+				</SortableContext>
+			</DndContext>
+		);
+	}
+
+	return (
+		<ThisSelect
+			menuPlacement="auto"
+			className="lazyblocks-component-select"
+			styles={selectStyles}
+			{...props}
 		/>
 	);
 }
