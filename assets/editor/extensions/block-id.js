bba46ace631a577dcee4be0826f2275cabc89a3d
Nikita <nkdevinfo@gmail.com>
2023-08-15 22:16:22 +0300

    changed block ID generation code
    fixed editor error when view block preview (resolves #295)

--- assets/editor/extensions/block-id.js
+++ assets/editor/extensions/block-id.js
@@ -7,16 +7,133 @@ import shorthash from 'shorthash';
  * WordPress dependencies.
  */
 import { addFilter } from '@wordpress/hooks';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useRef, useCallback } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 import { getBlockType } from '@wordpress/blocks';
-import { createHigherOrderComponent } from '@wordpress/compose';
+import { createHigherOrderComponent, useThrottle } from '@wordpress/compose';
 
-/**
- * List of used IDs to prevent duplicates.
- *
- * @type {Object}
- */
-const usedIds = {};
+function useBlockID(props) {
+	const { setAttributes, attributes, clientId, name } = props;
+	const blockSettings = getBlockType(name);
+	const didMountRef = useRef(false);
+
+	const { getBlocks } = useSelect((select) => {
+		return select('core/block-editor');
+	});
+
+	/**
+	 * Get recursive all blocks of the current page
+	 *
+	 * @param {boolean} blocks - block list
+	 *
+	 * @return {Array} block list
+	 */
+	const getAllBlocks = useCallback(
+		(blocks = false) => {
+			let result = [];
+
+			if (!blocks) {
+				blocks = getBlocks();
+			}
+
+			if (!blocks) {
+				return result;
+			}
+
+			blocks.forEach((data) => {
+				result.push(data);
+
+				if (data.innerBlocks && data.innerBlocks.length) {
+					result = [...result, ...getAllBlocks(data.innerBlocks)];
+				}
+			});
+
+			return result;
+		},
+		[getBlocks]
+	);
+
+	const onUpdate = useCallback(
+		(checkDuplicates) => {
+			let { blockId } = attributes;
+
+			if (!blockId || checkDuplicates) {
+				const usedIds = {};
+
+				// prevent unique ID duplication after block duplicated.
+				if (checkDuplicates) {
+					const allBlocks = getAllBlocks();
+					allBlocks.forEach((data) => {
+						if (
+							data.clientId &&
+							data.attributes &&
+							data.attributes.blockId
+						) {
+							usedIds[data.attributes.blockId] = data.clientId;
+
+							if (
+								data.clientId !== clientId &&
+								data.attributes.blockId === blockId
+							) {
+								blockId = '';
+							}
+						}
+					});
+				}
+
+				// prepare new block id.
+				if (clientId && !blockId && typeof blockId !== 'undefined') {
+					let ID = blockId || '';
+
+					// check if ID already exist.
+					let tryCount = 10;
+					while (
+						!ID ||
+						(typeof usedIds[ID] !== 'undefined' &&
+							usedIds[ID] !== clientId &&
+							tryCount > 0)
+					) {
+						ID = shorthash.unique(clientId);
+						tryCount -= 1;
+					}
+
+					if (ID && typeof usedIds[ID] === 'undefined') {
+						usedIds[ID] = clientId;
+					}
+
+					if (ID !== blockId) {
+						const newClass = `${name.replace('/', '-')}-${ID}`;
+
+						setAttributes({
+							blockId: ID,
+							blockUniqueClass: newClass,
+						});
+					}
+				}
+			}
+		},
+		[attributes, clientId, getAllBlocks, name, setAttributes]
+	);
+
+	const onUpdateThrottle = useThrottle(onUpdate, 60);
+
+	useEffect(() => {
+		if (!blockSettings.lazyblock) {
+			return;
+		}
+
+		// Did update.
+		if (didMountRef.current) {
+			onUpdateThrottle();
+
+			// Did mount.
+		} else {
+			didMountRef.current = true;
+
+			onUpdate(true);
+		}
+	}, [blockSettings, attributes, onUpdate, onUpdateThrottle]);
+}
 
 /**
  * Override the default edit UI to include a new block inspector control for
@@ -26,72 +143,13 @@ const usedIds = {};
  *
  * @return {string} Wrapped component.
  */
-const withUniqueBlockId = createHigherOrderComponent(
-	(BlockEdit) =>
-		function NewEdit(props) {
-			const { setAttributes, attributes, clientId, name } = props;
-			const { blockId } = attributes;
-			const blockSettings = getBlockType(name);
+const withBlockId = createHigherOrderComponent(
+	(BlockEdit) => (props) => {
+		useBlockID(props);
 
-			function maybeCreateBlockId() {
-				if (!blockSettings.lazyblock) {
-					return;
-				}
-
-				if (!blockId || usedIds[blockId] !== clientId) {
-					let newBlockId = '';
-
-					// check if ID already exist.
-					let tryCount = 10;
-					while (
-						!newBlockId ||
-						(typeof usedIds[newBlockId] !== 'undefined' &&
-							usedIds[newBlockId] !== clientId &&
-							tryCount > 0)
-					) {
-						newBlockId = shorthash.unique(clientId);
-						tryCount -= 1;
-					}
-
-					if (
-						newBlockId &&
-						typeof usedIds[newBlockId] === 'undefined'
-					) {
-						usedIds[newBlockId] = clientId;
-					}
-
-					if (newBlockId !== blockId) {
-						const newClass = `${name.replace(
-							'/',
-							'-'
-						)}-${newBlockId}`;
-
-						setAttributes({
-							blockId: newBlockId,
-							blockUniqueClass: newClass,
-						});
-					}
-				}
-			}
-
-			useEffect(() => {
-				// fix duplicated classes after block clone.
-				if (
-					clientId &&
-					attributes.blockId &&
-					typeof usedIds[attributes.blockId] === 'undefined'
-				) {
-					usedIds[attributes.blockId] = clientId;
-				}
-			}, [attributes.blockId, clientId]);
-
-			useEffect(() => {
-				maybeCreateBlockId();
-			});
-
-			return <BlockEdit {...props} />;
-		},
-	'withUniqueBlockId'
+		return <BlockEdit {...props} />;
+	},
+	'withBlockId'
 );
 
-addFilter('editor.BlockEdit', 'lazyblocks/uniqueBlockId', withUniqueBlockId);
+addFilter('editor.BlockEdit', 'lazyblocks/blockId', withBlockId);
