fa36ebac838f4cb0da5f69900541e6f42799a6e2
Nikita <nkdevinfo@gmail.com>
2024-03-06 14:52:14 +0300

    build

--- build/control-repeater.asset.php
+++ build/control-repeater.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '786384f47ae84a5500a5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0a88b2b89ced21825215');
