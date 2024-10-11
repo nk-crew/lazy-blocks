8e952715deaedce9f71687bf8ec4ead1821f65d5
Nikita <nkdevinfo@gmail.com>
2024-10-11 14:02:39 +0300

    build

--- build/control-gallery.asset.php
+++ build/control-gallery.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => '23107ba4c6cccbc03b5c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'afa207d371de7a3d04a8');
--- build/control-repeater.asset.php
+++ build/control-repeater.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2491b1e9b7275a108d3b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e3912133ff2892e30212');
