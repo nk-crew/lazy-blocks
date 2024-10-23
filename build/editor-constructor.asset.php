b0e4ee28f35410d716a25f5d5823162bacd33f3d
Fellan-91 <alexey.fellan@gmail.com>
2024-10-23 16:04:29 +0300

    build

--- build/editor-constructor.asset.php
+++ build/editor-constructor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '83dcb1f64c63b18059bd');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'b4d87d5d0f42afff605b');
