d9df31c149feb0bf972c395bb3b1bb5806489492
Fellan-91 <alexey.fellan@gmail.com>
2024-10-25 11:07:46 +0300

    build

--- build/control-date-time.asset.php
+++ build/control-date-time.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => 'a74be39354e0f29b0131');
+<?php return array('dependencies' => array('wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => 'a8057b816ff1aca30047');
