4e68c59ac951f820dce843c90a7695833dda0c98
Nikita <nkdevinfo@gmail.com>
2024-11-19 13:52:24 +0300

    build

--- build/control-checkbox.asset.php
+++ build/control-checkbox.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-hooks', 'wp-i18n'), 'version' => 'ff206dcad436b42d1c1c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '897e4435a3b520aa7f55');
