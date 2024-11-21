bc5122231e9825e1eb5b5130b5bbd9103a94de86
Nikita <nkdevinfo@gmail.com>
2024-11-21 15:01:43 +0300

    added dropdown for slug namespace button in constructor

--- assets/components/block-slug/index.js
+++ assets/components/block-slug/index.js
@@ -6,15 +6,53 @@ import './editor.scss';
 /**
  * WordPress dependencies.
  */
-import { BaseControl, TextControl } from '@wordpress/components';
+import {
+	BaseControl,
+	Button,
+	TextControl,
+	Dropdown,
+} from '@wordpress/components';
+
+import { __ } from '@wordpress/i18n';
+
+const { lazyblocksConstructorData } = window;
 
 export default function BlockSlug(props) {
 	return (
 		<BaseControl id={props.label} label={props.label || ''}>
 			<div className="lazyblocks-component-block-slug">
-				<div className="lazyblocks-component-block-slug-prefix">
-					lazyblock/
-				</div>
+				<Dropdown
+					className="lazyblocks-component-block-slug-prefix-dropdown"
+					contentClassName="lazyblocks-component-block-slug-prefix-dropdown-content"
+					popoverProps={{
+						placement: 'left-start',
+						offset: 36,
+						shift: true,
+					}}
+					renderToggle={({ isOpen, onToggle }) => (
+						<Button
+							className="lazyblocks-component-block-slug-prefix"
+							aria-expanded={isOpen}
+							onClick={onToggle}
+						>
+							lazyblock/
+						</Button>
+					)}
+					renderContent={() => (
+						<div className="lazyblocks-component-block-slug-prefix-dropdown lazyblocks-component-pro-notice">
+							Custom block slugs and collections are only
+							available in the Pro plugin.
+							<a
+								className="lazyblocks-component-pro-notice-btn"
+								target="_blank"
+								rel="noreferrer"
+								href={lazyblocksConstructorData.pro_url}
+							>
+								{__('Upgrade Now', 'lazy-blocks')}
+							</a>
+						</div>
+					)}
+				/>
 				<TextControl
 					{...{
 						...props,
--- build/editor-constructor.asset.php
+++ build/editor-constructor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '16ec8839b1a1269eb9d3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '7e281fd47d84cb1e761c');
