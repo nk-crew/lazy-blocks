4eb6d2a59e2e25d43d8b4173bc28bcd5ebe2284b
Nikita <nkdevinfo@gmail.com>
2024-11-24 19:13:41 +0300

    added `lzb.constructor.post-change` action when post changing

--- build/editor-constructor.asset.php
+++ build/editor-constructor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '7e281fd47d84cb1e761c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '5089af2dd1caff7e2148');
