31634f03eb377f1c7de97b8f81c625d1108ac762
Fellan-91 <alexey.fellan@gmail.com>
2025-11-26 21:05:40 +0300

    build: update built assets for URL control content placement

--- build/control-url.asset.php
+++ build/control-url.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-hooks'), 'version' => '8064fdca89d85429c3ef');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f3a80e2414b196466b4d');
