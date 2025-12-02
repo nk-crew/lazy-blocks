17aa24db97a4dc2c95dfde3656fdda6e87ab8fba
Nikita <nkdevinfo@gmail.com>
2025-12-02 17:17:45 +0300

    added more utils to store

--- assets/editor/store/utils/selectors.js
+++ assets/editor/store/utils/selectors.js
@@ -3,10 +3,22 @@
  */
 import getUID from '../../../utils/get-uid';
 import getControlTypeData from '../../../utils/get-control-type-data';
+import getControlValue from '../../../utils/get-control-value';
+import {
+	getSlugWithNamespace,
+	getSlugWithNamespaceDash,
+	getBlockClassName,
+	isValidSlug,
+} from '../../../utils/block-slug';
 
 export function get() {
 	return {
 		getUID,
 		getControlTypeData,
+		getControlValue,
+		getSlugWithNamespace,
+		getSlugWithNamespaceDash,
+		getBlockClassName,
+		isValidSlug,
 	};
 }
