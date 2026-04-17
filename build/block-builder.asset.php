d3dccb95c48f7428ebfea9665ff46bac9f0ffc2f
Fellan-91 <alexey.fellan@gmail.com>
2026-04-17 13:35:15 +0300

    Update built assets

--- build/block-builder.asset.php
+++ build/block-builder.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '64b91fdcb7ab6cfdd3de');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '47237b29d31135306c32');
