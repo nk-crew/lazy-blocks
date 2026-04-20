e36590be15dc842ef8ac09820e3ed884c3c462eb
Fellan-91 <alexey.fellan@gmail.com>
2026-04-20 13:12:29 +0300

    Update build artifacts for settings group defaults

--- build/block-builder.asset.php
+++ build/block-builder.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '738d8f44330a3282af5b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '5c2d85b69652c7e33b05');
