6998dbc9c5e83122062472c246aa9e16eefa28b5
Fellan-91 <alexey.fellan@gmail.com>
2026-04-20 13:05:21 +0300

    Update built assets for date time picker control

--- build/control-date-time.asset.php
+++ build/control-date-time.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => '45f2d1f6879da61432ac');
+<?php return array('dependencies' => array('wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => 'e0f71a81b8935e9d4923');
